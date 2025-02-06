82ed6b747abeab7e1d721d8d23d55330ed8feb64
Adarshcreater <160172392+Adarshcreater@users.noreply.github.com>
2025-02-06 23:57:41 +0530

    done

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".container")
+    .mouseenter(function () {
+      $(".card").stop().animate(
+        {
+          top: "-90px",
+        },
+        "slow"
+      );
+    })
+    .mouseleave(function () {
+      $(".card").stop().animate(
+        {
+          top: 0,
+        },
+        "slow"
+      );
+    });
+});
+function sendConfirmationEmail() {
+  $.post("send_email.php", {}, function (response) {
+      alert(response);
+  });
+}
